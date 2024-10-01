00bc66e3e4874eb0932b0b4218f138ed6b98c0eb
DinhQuy <hdquy2003@gmail.com>
2024-10-01 11:10:47 +0700

    bottom menu

--- app/src/main/java/com/example/bookverse/MainActivity.java
+++ app/src/main/java/com/example/bookverse/MainActivity.java
@@ -8,7 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.bookverse.Fragment.HomeFragment;
+import com.example.bookverse.Fragment.LibFragment;
+import com.example.bookverse.Fragment.PersonFragment;
+import com.example.bookverse.Fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.bottom_home){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                return true;
+            }
+            else if(item.getItemId() == R.id.bottom_search){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                return true;
+            }
+            else if(item.getItemId() == R.id.bottom_Lib){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LibFragment()).commit();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                return true;
+            }
+            else if(item.getItemId() == R.id.bottom_person){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PersonFragment()).commit();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                return true;
+            }
+            return false;
         });
     }
 }
