e28412878057f9852c8485ec09105ca9f84ef893
DinhQuy <hdquy2003@gmail.com>
2024-10-27 01:28:00 +0700

    set event search

--- app/src/main/java/com/example/bookverse/activities/SearchActivity.java
+++ app/src/main/java/com/example/bookverse/activities/SearchActivity.java
@@ -1,10 +1,15 @@
 package com.example.bookverse.activities;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,10 +23,13 @@ import com.example.bookverse.R;
 public class SearchActivity extends AppCompatActivity {
     TextView btnCancel;
     EditText inputSearch;
+    Drawable[] drawable;
+    Drawable rightIcon;
     ImageView btnSearch;
-    RecyclerView rvBook;
-    LinearLayout errorNotify;
+    LinearLayout errorNotify, layoutSearch, layoutHistory;
+    RecyclerView searchHistoryRv, search_searchRelativeRv, search_searchResult;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +42,32 @@ public class SearchActivity extends AppCompatActivity {
         });
 
         btnCancel = findViewById(R.id.search_cancelBtn);
+        btnCancel.setOnClickListener(view->{
+            finish();
+        });
         btnSearch = findViewById(R.id.search_searchBtn);
         inputSearch = findViewById(R.id.search_inputSearch);
-        rvBook = findViewById(R.id.recycleBook);
+        drawable = inputSearch.getCompoundDrawables();
+        inputSearch.setOnTouchListener((v, event)->{
+            if(event.getAction() == MotionEvent.ACTION_UP){
+                rightIcon = drawable[2];
+                if(rightIcon != null){
+                    //lay vi tri cua rightIcon
+                    int icontStart = inputSearch.getWidth() - inputSearch.getPaddingEnd() - rightIcon.getIntrinsicWidth();
+                    if(event.getX() >= icontStart){
+                        inputSearch.setText("" );
+                        Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                }
+                else{
+                    Toast.makeText(this, "RightIcon null", Toast.LENGTH_SHORT).show();
+                }
+                for (int i = 0; i < drawable.length; i++) {
+                    Log.d("Drawable Check", "Drawable " + i + ": " + (drawable[i] != null ? "Exists" : "Null"));
+                }
+            }
+            return false;
+        });
     }
 }
