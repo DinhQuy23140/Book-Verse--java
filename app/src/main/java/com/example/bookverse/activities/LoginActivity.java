d478b4b756cc6605f6ee1227204191f9dea261b3
DinhQuy <hdquy2003@gmail.com>
2024-10-28 23:35:27 +0700

    login func

--- app/src/main/java/com/example/bookverse/activities/LoginActivity.java
+++ app/src/main/java/com/example/bookverse/activities/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.bookverse.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.bookverse.MainActivity;
 import com.example.bookverse.R;
+import com.example.bookverse.utilities.Constants;
+import com.example.bookverse.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText login_editEmail, login_editPassword;
+    Button login_btnLogin;
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,28 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        login_editEmail = findViewById(R.id.login_editEmail);
+        login_editPassword = findViewById(R.id.login_edtPassword);
+        login_btnLogin = findViewById(R.id.log_btnLogin);
+        login_btnLogin.setOnClickListener(view->{
+            String email = login_editEmail.getText().toString();
+            String password = login_editPassword.getText().toString();
+            firestore.collection("users")
+                    .whereEqualTo(Constants.KEY_EMAIL, email)
+                    .whereEqualTo(Constants.KEY_PASSWORD, password)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful() && !task.getResult().isEmpty()){
+                            preferenceManager.putString(Constants.KEY_EMAIL, email);
+                            preferenceManager.putString(Constants.KEY_PASSWORD, password);
+                            preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                            Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(login);
+                            finish();
+                        }
+                    });
         });
     }
 }
---
+++ app/src/main/java/com/example/bookverse/utilities/Constants.java
@@ -0,0 +1,10 @@
+package com.example.bookverse.utilities;
+
+public class Constants {
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_ID = "id";
+    public static final String KEY_PREFERENCE_NAME = "bookVersePreference";
+    public static final String KEY_IS_SIGNED_IN = "signIn";
+}
